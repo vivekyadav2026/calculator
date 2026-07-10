2ad891065135b044f6189d43dbf67707f5d6edd9
ranjeet225 <ranjeetmaurya2033@gmail.com>
2026-07-10 23:20:35 +0530

    data

--- app/config/config.php
+++ app/config/config.php
@@ -2,18 +2,18 @@
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root (Adjust based on your local setup, e.g., http://localhost/calculator)
-// define('URLROOT', 'https://evfixer.in');
-define('URLROOT', 'http://localhost/calculator');
+define('URLROOT', 'https://evfixer.in');
+// define('URLROOT', 'http://localhost/calculator');
 // Site Name
 define('SITENAME', 'CalculatorTube');
 
 // Database Parameters
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'calculator_db');
-    
 // define('DB_HOST', 'localhost');
-// define('DB_USER', 'u798623491_calculator');
-// define('DB_PASS', 'Calculator2026@#$');
-// define('DB_NAME', 'u798623491_calculatordb');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'calculator_db');
+    
+define('DB_HOST', 'localhost');
+define('DB_USER', 'u798623491_calculator');
+define('DB_PASS', 'Calculator2026@#$');
+define('DB_NAME', 'u798623491_calculatordb');
